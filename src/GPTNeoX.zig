402fbb9a4bd389df55a3380dc376ffc2d0e9ad9e
LeRoyce Pearson <contact@leroycepearson.dev>
2023-04-14 20:18:28 -0600

    load using `mmap`

--- src/GPTNeoX.zig
+++ src/GPTNeoX.zig
@@ -1,4 +1,7 @@
 allocator: std.mem.Allocator,
+file: std.fs.File,
+file_buf: []align(std.mem.page_size) u8,
+ggml_mem_buffer: []u8,
 
 hparams: HParams,
 
@@ -72,15 +75,27 @@ pub const Layer = struct {
 pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Model {
     std.log.info("{s}: loading model from \"{}\"...", .{ @src().fn_name, std.zig.fmtEscapes(filename) });
 
-    const file = std.fs.cwd().openFile(filename, .{}) catch |err| {
+    var progress = std.Progress{};
+    var root_progress_node = progress.start(filename, 0);
+    root_progress_node.activate();
+    defer root_progress_node.end();
+
+    var model: Model = undefined;
+    model.allocator = allocator;
+
+    model.file = std.fs.cwd().openFile(filename, .{}) catch |err| {
         std.log.warn("{s}: failed to open \"{}\": {}", .{ @src().fn_name, std.zig.fmtEscapes(filename), err });
         return err;
     };
-    defer file.close();
+    errdefer model.file.close();
+    const file_len = try model.file.getEndPos();
+
+    const file_buf = try std.os.mmap(null, file_len, std.os.PROT.READ, std.os.MAP.SHARED, model.file.handle, 0);
+    errdefer std.os.munmap(file_buf);
 
     // verify magic
     {
-        const magic = try file.reader().readIntLittle(u32);
+        const magic = try model.file.reader().readIntLittle(u32);
         if (magic != 0x67676d6c) {
             std.log.warn("{s}: invalid model file \"{}\" (bad magic)", .{ @src().fn_name, std.zig.fmtEscapes(filename) });
             return error.BadMagic;
@@ -88,16 +103,14 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
     }
 
     // load hparams
-    var model: Model = undefined;
-    model.allocator = allocator;
 
-    model.hparams.n_vocab = try file.reader().readIntLittle(u32);
-    model.hparams.n_embd = try file.reader().readIntLittle(u32);
-    model.hparams.n_head = try file.reader().readIntLittle(u32);
-    model.hparams.n_layer = try file.reader().readIntLittle(u32);
-    model.hparams.n_rot = try file.reader().readIntLittle(u32);
-    model.hparams.use_parallel_residual = try file.reader().readIntLittle(u32);
-    model.hparams.f16 = try file.reader().readIntLittle(u32);
+    model.hparams.n_vocab = try model.file.reader().readIntLittle(u32);
+    model.hparams.n_embd = try model.file.reader().readIntLittle(u32);
+    model.hparams.n_head = try model.file.reader().readIntLittle(u32);
+    model.hparams.n_layer = try model.file.reader().readIntLittle(u32);
+    model.hparams.n_rot = try model.file.reader().readIntLittle(u32);
+    model.hparams.use_parallel_residual = try model.file.reader().readIntLittle(u32);
+    model.hparams.f16 = try model.file.reader().readIntLittle(u32);
 
     model.hparams.n_ctx = n_ctx;
 
@@ -116,10 +129,10 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
     model.tokens_arena = std.heap.ArenaAllocator.init(allocator);
     errdefer model.tokens_arena.deinit();
     for (0..model.hparams.n_vocab) |_| {
-        const len = try file.reader().readIntLittle(u32);
+        const len = try model.file.reader().readIntLittle(u32);
 
         const word = try model.tokens_arena.allocator().alloc(u8, len);
-        try file.reader().readNoEof(word);
+        try model.file.reader().readNoEof(word);
 
         try model.tokens.put(allocator, word, {});
     }
@@ -132,7 +145,7 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
         2 => c.GGML_TYPE_Q4_0,
         3 => c.GGML_TYPE_Q4_1,
         else => |w| {
-            std.log.warn("{s}: invalid model file \"{}\" (bad f16 value {d})", .{ @src().fn_name, std.zig.fmtEscapes(filename), w });
+            std.log.warn("{s}: invalid model model.file \"{}\" (bad f16 value {d})", .{ @src().fn_name, std.zig.fmtEscapes(filename), w });
             return error.BadF16Value;
         },
     };
@@ -140,15 +153,15 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
     var ctx_size: usize = 0;
 
     {
-        ctx_size += model.hparams.n_embd * c.ggml_type_size(c.c.GGML_TYPE_F32); // ln_f_g
-        ctx_size += model.hparams.n_embd * c.ggml_type_size(c.c.GGML_TYPE_F32); // ln_f_b
+        if (false) ctx_size += model.hparams.n_embd * c.ggml_type_size(c.c.GGML_TYPE_F32); // ln_f_g
+        if (false) ctx_size += model.hparams.n_embd * c.ggml_type_size(c.c.GGML_TYPE_F32); // ln_f_b
 
-        ctx_size += model.hparams.n_embd * model.hparams.n_vocab * c.ggml_type_size(wtype); // wte
+        if (false) ctx_size += model.hparams.n_embd * model.hparams.n_vocab * c.ggml_type_size(wtype); // wte
 
-        ctx_size += model.hparams.n_embd * model.hparams.n_vocab * c.ggml_type_size(wtype); // lmh_g
+        if (false) ctx_size += model.hparams.n_embd * model.hparams.n_vocab * c.ggml_type_size(wtype); // lmh_g
         // ctx_size +=        n_vocab*c.ggml_type_sizef(c.GGML_TYPE_F32); // lmh_b
 
-        { // Transformer layers
+        if (false) { // Transformer layers
             { // Layernorms
                 ctx_size += model.hparams.n_layer * (model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32)); // input_layernorm_weight
                 ctx_size += model.hparams.n_layer * (model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32)); // input_layernorm_bias
@@ -179,8 +192,8 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
             }
         }
 
-        ctx_size += model.hparams.n_ctx * model.hparams.n_layer * model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32); // memory_k
-        ctx_size += model.hparams.n_ctx * model.hparams.n_layer * model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32); // memory_v
+        if (false) ctx_size += model.hparams.n_ctx * model.hparams.n_layer * model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32); // memory_k
+        if (false) ctx_size += model.hparams.n_ctx * model.hparams.n_layer * model.hparams.n_embd * c.ggml_type_size(c.GGML_TYPE_F32); // memory_v
 
         ctx_size += (6 + 16 * model.hparams.n_layer) * 256; // object overhead
 
@@ -188,12 +201,12 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
     }
 
     // create the ggml context
-    const mem_buffer = try allocator.alloc(u8, ctx_size);
-    errdefer allocator.free(mem_buffer);
+    model.ggml_mem_buffer = try allocator.alloc(u8, ctx_size);
+    errdefer allocator.free(model.ggml_mem_buffer);
     var params = c.ggml_init_params{
-        .mem_size = ctx_size,
-        .mem_buffer = mem_buffer.ptr,
-        .no_alloc = false,
+        .mem_size = model.ggml_mem_buffer.len,
+        .mem_buffer = model.ggml_mem_buffer.ptr,
+        .no_alloc = true,
     };
 
     model.ctx = c.ggml_init(params) orelse {
@@ -202,9 +215,9 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
     };
 
     // prepare memory for the weights
+    model.layers = try allocator.alloc(Layer, model.hparams.n_layer);
+    errdefer allocator.free(model.layers);
     {
-        model.layers = try allocator.alloc(Layer, model.hparams.n_layer);
-
         model.wte = c.ggml_new_tensor_2d(model.ctx, wtype, model.hparams.n_embd, model.hparams.n_vocab);
 
         model.ln_f_g = c.ggml_new_tensor_1d(model.ctx, c.GGML_TYPE_F32, model.hparams.n_embd);
@@ -263,28 +276,32 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
         defer name_buffer.deinit(allocator);
 
         while (true) {
-            const n_dims = try file.reader().readIntLittle(i32);
-            const name_length = try file.reader().readIntLittle(i32);
-            const ftype = try file.reader().readIntLittle(i32);
-
-            if (try file.getPos() == try file.getEndPos()) {
+            if (try model.file.getPos() == try model.file.getEndPos()) {
                 break;
             }
 
-            if (n_dims != 2) {
+            const n_dims = try model.file.reader().readIntLittle(u32);
+            const name_length = try model.file.reader().readIntLittle(u32);
+            const ftype = try model.file.reader().readIntLittle(u32);
+
+            if (n_dims > 2) {
                 std.log.warn("{s}: n_dims greater than 2 unimplemented, n_dims = {d}", .{ @src().fn_name, n_dims });
                 return error.Unimplemented;
             }
 
             var nelements: u32 = 1;
             var shape = [2]u32{ 1, 1 };
-            for (&shape) |*ne| {
-                ne.* = try file.reader().readIntLittle(u32);
+            for (shape[0..n_dims]) |*ne| {
+                ne.* = try model.file.reader().readIntLittle(u32);
                 nelements *= ne.*;
             }
 
-            try name_buffer.resize(allocator, name_length);
-            try file.reader().readNoEof(name_buffer.items);
+            try name_buffer.resize(allocator, @intCast(usize, name_length));
+            try model.file.reader().readNoEof(name_buffer.items);
+
+            var tensor_progress_node = progress.start(name_buffer.items, 0);
+            tensor_progress_node.activate();
+            defer tensor_progress_node.end();
 
             // TODO: Parse name
             var tensor: ?**c.ggml_tensor = null;
@@ -324,20 +341,34 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
                     tensor = &layer.c_attn_proj_w;
                 } else if (std.mem.eql(u8, layer_desc, "attention.dense.bias")) {
                     tensor = &layer.c_attn_proj_bias;
-                } else if (std.mem.eql(u8, layer_desc, "attention.dense_h_to_4h.weight")) {
+                } else if (std.mem.eql(u8, layer_desc, "mlp.dense_h_to_4h.weight")) {
                     tensor = &layer.c_mlp_fc_w;
-                } else if (std.mem.eql(u8, layer_desc, "attention.dense_h_to_4h.bias")) {
-                    tensor = &layer.c_mlp_fc_w;
-                } else if (std.mem.eql(u8, layer_desc, "attention.dense_4h_to_h.weight")) {
+                } else if (std.mem.eql(u8, layer_desc, "mlp.dense_h_to_4h.bias")) {
+                    tensor = &layer.c_mlp_fc_b;
+                } else if (std.mem.eql(u8, layer_desc, "mlp.dense_4h_to_h.weight")) {
                     tensor = &layer.c_mlp_proj_w_trans;
-                } else if (std.mem.eql(u8, layer_desc, "attention.dense_4h_to_h.bias")) {
+                } else if (std.mem.eql(u8, layer_desc, "mlp.dense_4h_to_h.bias")) {
                     tensor = &layer.c_mlp_proj_b;
                 }
-            } else if (std.mem.eql(u8, layer_or_other, "hello, world")) {}
+            } else if (std.mem.eql(u8, layer_or_other, "embed_in")) {
+                const tensor_type_str = name_segment_iter.rest();
+                if (std.mem.eql(u8, tensor_type_str, "weight")) {
+                    tensor = &model.wte;
+                }
+            } else if (std.mem.eql(u8, layer_or_other, "final_layer_norm")) {
+                const tensor_type_str = name_segment_iter.rest();
+                if (std.mem.eql(u8, tensor_type_str, "weight")) {
+                    tensor = &model.ln_f_g;
+                } else if (std.mem.eql(u8, tensor_type_str, "bias")) {
+                    tensor = &model.ln_f_b;
+                }
+            } else if (std.mem.eql(u8, name_buffer.items, "embed_out.weight")) {
+                tensor = &model.lmh_g;
+            }
 
             if (tensor == null) {
                 std.log.warn("{s}: unknown tensor \"{}\"", .{ @src().fn_name, std.zig.fmtEscapes(name_buffer.items) });
-                continue;
+                return error.BadTensorName;
             }
 
             if (c.ggml_nelements(tensor.?.*) != nelements) {
@@ -356,26 +387,27 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
                 2 => c.ggml_type_size(c.GGML_TYPE_Q4_0),
                 3 => c.ggml_type_size(c.GGML_TYPE_Q4_1),
                 else => |f| {
-                    std.log.warn(" {s}: unknown ftype {} in model file", .{ @src().fn_name, f });
+                    std.log.warn(" {s}: unknown ftype {} in model model.file", .{ @src().fn_name, f });
                     return error.UnknownFType;
                 },
             };
 
             if ((nelements * bpe_size) / @intCast(usize, c.ggml_blck_size(tensor.?.*.type)) != c.ggml_nbytes(tensor.?.*)) {
-                std.log.warn(" {s}: tensor \"{}\" has wrong size in model file, expected {d}, got {d}", .{ @src().fn_name, std.zig.fmtEscapes(name_buffer.items), c.ggml_nbytes(tensor.?.*), nelements * bpe_size });
+                std.log.warn(" {s}: tensor \"{}\" has wrong size in model model.file, expected {d}, got {d}", .{ @src().fn_name, std.zig.fmtEscapes(name_buffer.items), c.ggml_nbytes(tensor.?.*), nelements * bpe_size });
                 return error.InvalidModelSize;
             }
 
-            try file.reader().readNoEof(@ptrCast([*]u8, tensor.?.*.data)[0..c.ggml_nbytes(tensor.?.*)]);
+            const offset = try model.file.getPos();
+            const tensor_byte_size = c.ggml_nbytes(tensor.?.*);
+
+            tensor.?.*.data = model.file_buf[offset..][0..tensor_byte_size].ptr;
+
+            try model.file.seekBy(@intCast(i64, tensor_byte_size));
+            // try model.file.reader().readNoEof(@ptrCast([*]u8, tensor.?.*.data)[0..]);
 
             total_size += c.ggml_nbytes(tensor.?.*);
             n_tensors += 1;
-            if (n_tensors % 8 == 0) {
-                std.debug.print(".", .{});
-            }
         }
-
-        std.debug.print(" done\n", .{});
 
         std.log.info("{s}: model size = {d:8.2} MB / num_tensors = {}", .{ @src().fn_name, @intToFloat(f32, total_size) / 1024.0 / 1024.0, n_tensors });
     }
@@ -384,6 +416,8 @@ pub fn load(allocator: std.mem.Allocator, filename: []const u8, n_ctx: u32) !Mod
 }
 
 pub fn deinit(this: *@This()) void {
+    this.allocator.free(this.layers);
+    this.allocator.free(this.ggml_mem_buffer);
     this.tokens.deinit(this.allocator);
     this.tokens_arena.deinit();
 }
